d651c55fdf50fb61fa99768b6ec8af98ddd0f5c1
kawao3 <rkawa0707@gmail.com>
2021-02-08 22:25:27 +0900

    ass

--- ass.cpp
+++ ass.cpp
@@ -2,14 +2,9 @@
 using namespace std;
 int main()
 {
-    long long N;
-    cin >> N;
-    for (int i = 59; i >= 0; i--)
-    {
-        if ((N >> i & 1) == 1)
-        {
-            cout << ((long long)1 << i) << endl;
-            break;
-        }
-    }
+    long long n;
+    cin >> n;
+
+    long long sum = (n * (n - 1)) / 2;
+    cout << sum << endl;
 }
