e17c0304ed5b10a7bee5d54d99049b8af83fdc98
kawao3 <rkawa0707@gmail.com>
2021-02-08 22:26:41 +0900

    as

--- as.cpp
+++ as.cpp
@@ -6,31 +6,19 @@ const long long INF = 1LL << 60;
 
 typedef pair<ll, ll> Pair;
 
-long long minn = 1e15; // 最小値を求めるための値
-
-int fact(int n)
-{
-    if (n == 0)
-        return 1;
-    else
-        return n * fact(n - 1);
-}
-
-int combination(int n, int r)
-{
-    return fact(n) / (fact(r) * fact(n - r));
-}
 int main()
 {
 
-    int n, r;
+    long long n;
+    cin >> n;
 
-    printf("n:");
-    scanf("%d", &n);
-    printf("r:");
-    scanf("%d", &r);
+    set<long long> a;
 
-    printf("%d C %d = %d\n", n, r, combination(n, r));
-
-    return 0;
+    for (long long i = 1; i <= n; i++)
+    {
+        if (i & n != 0)
+        {
+            a.insert(i);
+        }
+    }
 }
